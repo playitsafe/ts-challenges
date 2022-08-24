926a67ec459b85e412d3034ccf8187e4c59455bd
playitsafe <playitsafe@qq.com>
2022-08-25 01:41:56 +0800

    finished 00009-medium-deep readonly

--- challenges/00003-medium-omit/template.ts
+++ challenges/00003-medium-omit/template.ts
@@ -5,4 +5,3 @@
 type MyOmit<T, K extends keyof T> = {
   [KEY in Exclude<keyof T, K>]: T[KEY]
 }
-
---
+++ challenges/00009-medium-deep-readonly/template.ts
@@ -0,0 +1,4 @@
+type DeepReadonly<T> = {
+  readonly [KEY in keyof T]: T[KEY] extends Function ? T[KEY] :  T[KEY] extends object ? DeepReadonly<T[KEY]>
+  : T[KEY]
+}
