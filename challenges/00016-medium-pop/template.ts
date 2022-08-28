17a15b3687d40df5ff6f24a77407ccaa7a1994f5
playitsafe <playitsafe@qq.com>
2022-08-28 19:24:17 +0800

    add 00016-medium-pop

---
+++ challenges/00016-medium-pop/template.ts
@@ -0,0 +1,7 @@
+// type Pop<T extends any[]> = any
+
+//----------
+
+// type Pop<T extends any[]> = T extends [...infer REST, infer LAST] ? REST : never
+
+// type Pop<T extends any[], A extends any[] = []> = T extends [infer E, ...infer R] ? R['length'] extends 0 ? A : Pop<R, [...A, E]> : A;
